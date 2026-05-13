7c3eea3f300b4febeecd090fbdbb54ec00fbaba5
harissonm <harissonmota99@gmail.com>
2026-05-13 14:42:40 +0000

    Adiciona aplicacao de configuracao no switch com Netmiko

--- appsw.py
+++ appsw.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from netmiko import ConnectHandler
 
 app = Flask(__name__)
 
@@ -14,6 +15,20 @@ def gerar_comandos(hostname, vlans):
         comandos.append("exit")
 
     return comandos
+def aplicar_no_switch(comandos):
+    switch = {
+        "device_type": "cisco_ios",
+        "host": "192.168.254.34",
+        "username": "harisson",
+        "password": "m0t@2026",
+    }
+
+    conexao = ConnectHandler(**switch)
+    resultado = conexao.send_config_set(comandos)
+    conexao.save_config()
+    conexao.disconnect()
+
+    return resultado
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -32,6 +47,12 @@ def index():
         comandos = gerar_comandos(hostname, vlans)
 
         resultado = "\n".join(comandos)
+    try:
+        retorno_switch = aplicar_no_switch(comandos)
+        resultado += "\n\nConfiguração aplicada no switch com sucesso!\n\n"
+        resultado += retorno_switch
+    except Exception as erro:
+        resultado += f"\n\nErro ao aplicar no switch:\n{erro}"
 
     return f"""
     <html>
